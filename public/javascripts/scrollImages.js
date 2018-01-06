8ed9343fb2cbac657f5b815fed372506e64490ad
anandchandra50 <anandchandra50@gmail.com>
2018-01-06 15:40:58 -0800

    apple logo, bar

--- public/javascripts/scrollImages.js
+++ public/javascripts/scrollImages.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', function() {
-  console.log('All assets are loaded')
 
   var imageURLForIndex = ['/images/hzscreen.png', '/images/tunerscreen.png',
     '/images/playbackscreen.png', '/images/savedscreen.png', '/images/calibratescreen.png'
@@ -10,24 +9,35 @@ window.addEventListener('load', function() {
   var imageIndex = 1; //start at 1 because image is already set to first
   var imageChangeTrack = 0; //0 means fade out image, 1 means fade out second-image
 
-  var image = document.getElementById('image');
+  var image = document.getElementById('image'); // two images are used to cross-fade (fade one out and the other in) for animation
   var secondImage = document.getElementById('second-image');
   secondImage.opacity = 0
 
-  var details = document.getElementById('details');
+  var details = document.getElementById('details'); // two details are used to cross-fade (fade one out and the other in) for animation
   var secondDetails = document.getElementById('second-details');
   secondDetails.opacity = 0
 
+  var border = document.getElementById('border'); //border that moves along bottom with slides
+  var currentLeft = 0; //current position of border (in vw)
+  var newLeft = 20; //new position of border (in vw)
+
   setInterval(function() {
     // method to be executed;
-    if (imageChangeTrack % 2 === 0) {
-    	secondImage.src = imageURLForIndex[imageIndex];
-    	secondDetails.innerHTML = detailTextForIndex[imageIndex];
+    if (imageChangeTrack % 2 === 0) { //determine whether to fade out first or second image
+    	//fade out first
+    	secondImage.src = imageURLForIndex[imageIndex]; //second is still 0 opacity, update image and fade it in
+    	secondDetails.innerHTML = detailTextForIndex[imageIndex]; //same for detail
 
-    	_('image').fade('out', 200);
+    	_('image').fade('out', 200); //cross-fade
     	_('details').fade('out', 200);
     	_('second-image').fade('in', 200);
     	_('second-details').fade('in', 200);
+
+    	//move bottom border and update position
+    	move(border, currentLeft, newLeft, 500);
+    	currentLeft = newLeft;
+    	newLeft += 20; //currently 5 slides, so each one is 1/5 of the whole screen
+    	newLeft = newLeft % 100;
 
     	//image is out so change image url
     	imageIndex++;
@@ -35,32 +45,45 @@ window.addEventListener('load', function() {
     	imageChangeTrack++;
     	
     } else {
-		image.src = imageURLForIndex[imageIndex];
-		details.innerHTML = detailTextForIndex[imageIndex];
+    	//fade out second
+		image.src = imageURLForIndex[imageIndex]; //first is still 0 opacity, update image and fade it in
+		details.innerHTML = detailTextForIndex[imageIndex]; //same for detail
 
-    	_('second-image').fade('out', 200);
+    	_('second-image').fade('out', 200); //cross-fade
     	_('second-details').fade('out', 200);
     	_('image').fade('in', 200);
     	_('details').fade('in', 200);
+
+    	//move bottom border and update position
+    	move(border, currentLeft, newLeft, 500);
+    	currentLeft = newLeft;
+    	newLeft += 20; //currently 5 slides, so each one is 1/5 of the whole screen
+    	newLeft = newLeft % 100;
+
     	//second-image is out so change second-image url
     	imageIndex++;
     	imageIndex = imageIndex % 5;
     	imageChangeTrack++;
     }
 
-  }, 5000); //10 seconds
+  }, 5000); //5 seconds
 })
 
-function myClick() {
-  setTimeout(
-    function() {
-      document.getElementById('div1').style.display='none';
-      document.getElementById('div2').style.display='none';
-    }, 5000);
+///Animate move element from currentLeft to newLeft over duration milliseconds
+function move(elem, currentLeft, newLeft, duration) {
+    var left = currentLeft;
+    var refreshRate = 10; //arbitrary refresh rate -- 100 times/second
+    var increment = (newLeft - currentLeft) / (duration/refreshRate) //number of vw that should increase in each frame
+    function frame() {
+        left += increment;  // update left
+        elem.style.left = left + 'vw'; // update frame position
+        if (Math.round(left) === newLeft)  // check finish condition
+            clearInterval(id);
+    }
+    var id = setInterval(frame, refreshRate); // draw every 10ms
 }
 
-
-
+///helper function for fade
 function _(el) {
   if (!(this instanceof _)) {
     return new _(el);
@@ -68,6 +91,7 @@ function _(el) {
   this.el = document.getElementById(el);
 }
 
+///Fade image in or out over certain duration
 _.prototype.fade = function fade(type, ms) {
   var isIn = type === 'in',
     opacity = isIn ? 0 : 1,
