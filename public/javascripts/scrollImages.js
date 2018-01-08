14239e763c718c586c49ae5be49db5dbcea5205e
anandchandra50 <anandchandra50@gmail.com>
2018-01-07 18:21:29 -0800

    heroku launch

--- public/javascripts/scrollImages.js
+++ public/javascripts/scrollImages.js
@@ -3,8 +3,9 @@ window.addEventListener('load', function() {
   var imageURLForIndex = ['/images/hzscreen.png', '/images/tunerscreen.png',
     '/images/playbackscreen.png', '/images/savedscreen.png', '/images/calibratescreen.png'
   ]; //store url depending on index
-  var detailTextForIndex = ['never miss a note again', 'learn from real-time tuning...', '...or view past performances', 
-  	'keep recordings forever', 'tune relative to your instrument'];
+  var detailTextForIndex = ['never miss a note again', 'learn from real-time tuning...', '...or view past performances',
+    'keep recordings forever', 'tune relative to your instrument'
+  ];
 
   var imageIndex = 1; //start at 1 because image is already set to first
   var imageChangeTrack = 0; //0 means fade out image, 1 means fade out second-image
@@ -24,63 +25,63 @@ window.addEventListener('load', function() {
   setInterval(function() {
     // method to be executed;
     if (imageChangeTrack % 2 === 0) { //determine whether to fade out first or second image
-    	//fade out first
-    	secondImage.src = imageURLForIndex[imageIndex]; //second is still 0 opacity, update image and fade it in
-    	secondDetails.innerHTML = detailTextForIndex[imageIndex]; //same for detail
+      //fade out first
+      secondImage.src = imageURLForIndex[imageIndex]; //second is still 0 opacity, update image and fade it in
+      secondDetails.innerHTML = detailTextForIndex[imageIndex]; //same for detail
 
-    	_('image').fade('out', 200); //cross-fade
-    	_('details').fade('out', 200);
-    	_('second-image').fade('in', 200);
-    	_('second-details').fade('in', 200);
+      _('image').fade('out', 200); //cross-fade
+      _('details').fade('out', 200);
+      _('second-image').fade('in', 200);
+      _('second-details').fade('in', 200);
 
-    	//move bottom border and update position
-    	move(border, currentLeft, newLeft, 500);
-    	currentLeft = newLeft;
-    	newLeft += 20; //currently 5 slides, so each one is 1/5 of the whole screen
-    	newLeft = newLeft % 100;
+      //move bottom border and update position
+      move(border, currentLeft, newLeft, 500);
+      currentLeft = newLeft;
+      newLeft += 20; //currently 5 slides, so each one is 1/5 of the whole screen
+      newLeft = newLeft % 100;
 
-    	//image is out so change image url
-    	imageIndex++;
-    	imageIndex = imageIndex % 5;
-    	imageChangeTrack++;
-    	
+      //image is out so change image url
+      imageIndex++;
+      imageIndex = imageIndex % 5;
+      imageChangeTrack++;
+
     } else {
-    	//fade out second
-		image.src = imageURLForIndex[imageIndex]; //first is still 0 opacity, update image and fade it in
-		details.innerHTML = detailTextForIndex[imageIndex]; //same for detail
+      //fade out second
+      image.src = imageURLForIndex[imageIndex]; //first is still 0 opacity, update image and fade it in
+      details.innerHTML = detailTextForIndex[imageIndex]; //same for detail
 
-    	_('second-image').fade('out', 200); //cross-fade
-    	_('second-details').fade('out', 200);
-    	_('image').fade('in', 200);
-    	_('details').fade('in', 200);
+      _('second-image').fade('out', 200); //cross-fade
+      _('second-details').fade('out', 200);
+      _('image').fade('in', 200);
+      _('details').fade('in', 200);
 
-    	//move bottom border and update position
-    	move(border, currentLeft, newLeft, 500);
-    	currentLeft = newLeft;
-    	newLeft += 20; //currently 5 slides, so each one is 1/5 of the whole screen
-    	newLeft = newLeft % 100;
+      //move bottom border and update position
+      move(border, currentLeft, newLeft, 500);
+      currentLeft = newLeft;
+      newLeft += 20; //currently 5 slides, so each one is 1/5 of the whole screen
+      newLeft = newLeft % 100;
 
-    	//second-image is out so change second-image url
-    	imageIndex++;
-    	imageIndex = imageIndex % 5;
-    	imageChangeTrack++;
+      //second-image is out so change second-image url
+      imageIndex++;
+      imageIndex = imageIndex % 5;
+      imageChangeTrack++;
     }
 
   }, 5000); //5 seconds
-})
+});
 
 ///Animate move element from currentLeft to newLeft over duration milliseconds
 function move(elem, currentLeft, newLeft, duration) {
-    var left = currentLeft;
-    var refreshRate = 10; //arbitrary refresh rate -- 100 times/second
-    var increment = (newLeft - currentLeft) / (duration/refreshRate) //number of vw that should increase in each frame
-    function frame() {
-        left += increment;  // update left
-        elem.style.left = left + 'vw'; // update frame position
-        if (Math.round(left) === newLeft)  // check finish condition
-            clearInterval(id);
-    }
-    var id = setInterval(frame, refreshRate); // draw every 10ms
+  var left = currentLeft;
+  var refreshRate = 10; //arbitrary refresh rate -- 100 times/second
+  var increment = (newLeft - currentLeft) / (duration / refreshRate) //number of vw that should increase in each frame
+  function frame() {
+    left += increment; // update left
+    elem.style.left = left + 'vw'; // update frame position
+    if (Math.round(left) === newLeft) // check finish condition
+      clearInterval(id);
+  }
+  var id = setInterval(frame, refreshRate); // draw every 10ms
 }
 
 ///helper function for fade
