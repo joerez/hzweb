37c6e30aabfc2f37b59bb6d35edbbc0d61714745
anandchandra50 <anandchandra50@gmail.com>
2018-01-07 19:14:14 -0800

    changes

--- public/javascripts/scrollImages.js
+++ public/javascripts/scrollImages.js
@@ -56,7 +56,7 @@ window.addEventListener('load', function() {
       _('details').fade('in', 200);
 
       //move bottom border and update position
-      move(border, currentLeft, newLeft, 200); //change 500
+      move(border, currentLeft, newLeft, 300); //change 500
       currentLeft = newLeft;
       newLeft += 20; //currently 5 slides, so each one is 1/5 of the whole screen
       newLeft = newLeft % 100;
@@ -67,7 +67,7 @@ window.addEventListener('load', function() {
       imageChangeTrack++;
     }
 
-  }, 2000); //5 seconds
+  }, 3000); //4 seconds
 });
 
 ///Animate move element from currentLeft to newLeft over duration milliseconds
